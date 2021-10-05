d0cf12a2d620307879ced5cadd719ee1c0ac0b3e
Jair Partida <jairpartida1@gmail.com>
2021-10-04 23:10:41 -0600

    counter app done

--- basic-js/counter-app/app.js
+++ basic-js/counter-app/app.js
@@ -1,13 +1,16 @@
 let count = 0;
 let countEl = document.getElementById('count-el')
-
+let saveEl = document.getElementById('save-el')
 
 function increment() {
-    count = count + 1
-    countEl.innerHTML = count;
+    count += 1
+    countEl.textContent = count;
     //console.log(count);
 }
 
 function save() {
-    console.log(count)
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
 }
