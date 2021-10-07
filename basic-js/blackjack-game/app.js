a4364fc41bc37732d1c8838db121d86e25710230
Jair Partida <jairpartida1@gmail.com>
2021-10-06 22:43:52 -0600

    almost done  blackjack game

--- basic-js/blackjack-game/app.js
+++ basic-js/blackjack-game/app.js
@@ -1,20 +1,37 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard,secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById('message-el')
 let sumEl = document.getElementById('sum-el')
 let cardsEl = document.getElementById('cards-el')
 
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random()*13) + 1
+    if (randomCard > 10) {
+        return 10
+    } else if (randomCard === 1) {
+        return 11
+    } else {
+        return randomCard
+    }  
+}
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " | "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -31,7 +48,8 @@ function renderGame() {
 
 function newCard(){
     console.log("Drawing a new card")
-    let card = 7
+    let card = getRandomCard()
     sum += card
+    cards.push(card)
     renderGame()
 }
