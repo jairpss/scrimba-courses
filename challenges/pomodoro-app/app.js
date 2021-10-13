cb959a7305f3ca9fc762c5b80b753262f178aa9c
Jair Partida <jairpartida1@gmail.com>
2021-10-12 22:49:47 -0600

    setting up the app

---
+++ challenges/pomodoro-app/app.js
@@ -0,0 +1,4 @@
+const btnWorker = document.getElementById("work-btn")
+const shortBreakBtn = document.getElementById("short-break-btn")
+const longBreakBtn = document.getElementById("long-break-btn")
+
