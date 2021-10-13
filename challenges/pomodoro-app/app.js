33ef5ea06502393eef0149b8ab2777d073f14f5d
Jair Partida <jairpartida1@gmail.com>
2021-10-12 22:52:24 -0600

    adding event listeners

--- challenges/pomodoro-app/app.js
+++ challenges/pomodoro-app/app.js
@@ -2,3 +2,10 @@ const btnWorker = document.getElementById("work-btn")
 const shortBreakBtn = document.getElementById("short-break-btn")
 const longBreakBtn = document.getElementById("long-break-btn")
 
+btnWorker.addEventListener("click", startTimer)
+shortBreakBtn.addEventListener("click", startTimer)
+longBreakBtn.addEventListener("click", startTimer)
+
+function startTimer() {
+    console.log("button clicked")
+}
