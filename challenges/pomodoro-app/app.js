a2077dc14c0b5d10e65f29a1a3153631830d13b7
Jair Partida <jairpartida1@gmail.com>
2021-10-13 21:55:27 -0600

    creating the function

--- challenges/pomodoro-app/app.js
+++ challenges/pomodoro-app/app.js
@@ -1,11 +1,14 @@
 const btnWorker = document.getElementById("work-btn")
 const shortBreakBtn = document.getElementById("short-break-btn")
 const longBreakBtn = document.getElementById("long-break-btn")
+let timeDisplay = document.getElementById("time-display")
 
 btnWorker.addEventListener("click", startTimer)
 shortBreakBtn.addEventListener("click", startTimer)
 longBreakBtn.addEventListener("click", startTimer)
 
 function startTimer() {
-    console.log("button clicked")
+    const workTimeSeconds = 1500
+    const time = 0
+    timeDisplay.innerText = `${Math.floor(workTimeSeconds / 60)}:${workTimeSeconds % 60}`
 }
