478ffd6e9366cefdf4edfd7aa201d36e127fcfb7
Jair Partida <jairpartida1@gmail.com>
2021-10-16 23:21:43 -0600

    app finished

--- challenges/pomodoro-app/app.js
+++ challenges/pomodoro-app/app.js
@@ -2,7 +2,6 @@ const workBtn = document.getElementById("work-btn")
 const shortBreakBtn = document.getElementById("short-break-btn")
 const longBreakBtn = document.getElementById("long-break-btn")
 let timeDisplay = document.getElementById("time-display")
-let typeOfCountdown = ""
 let timeout;
 
 workBtn.addEventListener("click",function() { startTimer(25) })
@@ -10,7 +9,10 @@ shortBreakBtn.addEventListener("click", function() { startTimer(5) })
 longBreakBtn.addEventListener("click", function() { startTimer(15) })
 
 function startTimer(time) {
-    clearInterval(timeout)
+    if (typeof timeout !== undefined) {
+        clearInterval(timeout)
+    }
+    
     let allowedTime = time * 60
     showTime(allowedTime)
     timeout = setInterval(function() {
@@ -26,7 +28,14 @@ function startTimer(time) {
 }
 
 function showTime(allowedTime) {
-    let time = `${Math.floor(allowedTime / 60)}:${allowedTime % 60}`
+    let minutes = pad(Math.floor(allowedTime / 60))
+    let seconds = pad(allowedTime % 60)
+
+    let time = `${minutes}:${seconds}`
     document.title = `Time remaining: ${time}`
     timeDisplay.innerText = time
+}
+
+function pad(number) {
+    return number < 10 ? `0${number.toString()}` : number
 }
