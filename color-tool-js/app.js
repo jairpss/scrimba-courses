ed69b769dfde935e0f5d5f1a74314314c3102fc7
Jair Partida <jairpartida1@gmail.com>
2021-10-19 22:36:28 -0600

    alter color by percentage

--- color-tool-js/app.js
+++ color-tool-js/app.js
@@ -1,5 +1,7 @@
 const hexInput = document.getElementById('hexInput')
 const inputColor = document.getElementById('inputColor')
+const sliderText = document.getElementById('sliderText')
+const slider = document.getElementById('slider')
 
 hexInput.addEventListener('keyup', () => {
     const hex = hexInput.value
@@ -10,6 +12,10 @@ hexInput.addEventListener('keyup', () => {
     inputColor.style.backgroundColor = "#" + strippedHex
 })
 
+slider.addEventListener('input', () => {
+    sliderText.textContent = `${slider.value}%`
+})
+
 const isValidHex = (hex) => {
     if(!hex) return false
 
@@ -17,7 +23,7 @@ const isValidHex = (hex) => {
     return strippedHex.length === 3 || strippedHex.length === 6
 }
 
-const convertHex = (hex) => {
+const convertHexToRGB = (hex) => {
     if(!isValidHex(hex)) return null
 
     let strippedHex = hex.replace('#', '')
@@ -34,3 +40,24 @@ const convertHex = (hex) => {
 
     return {r,g,b}
 }
+
+const convertRGBToHex = (r,g,b) => {
+    const firstPair = ("0" + r.toString(16)).slice(-2)
+    const secondPair = ("0" + g.toString(16)).slice(-2)
+    const thirdPair = ("0" + b.toString(16)).slice(-2)
+
+    const hex = "#" + firstPair + secondPair + thirdPair
+    return hex
+}
+
+const alterColor = (hex, percentage) => {
+    const {r,g,b} = convertHexToRGB(hex)
+    
+    const amount = Math.floor((percentage/100) * 255)
+    
+    const newR = r + amount
+    const newG = g + amount
+    const newB = b + amount
+    console.log(newR, newG, newB)
+    return convertRGBToHex(newR, newG, newB)
+  }
