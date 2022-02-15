1a202e0908eba157df6e962ed1dace17690ab4b2
Jair Partida <jairpartida1@gmail.com>
2022-02-14 23:55:35 -0700

    main function done

--- gift-selector-challenge/app.js
+++ gift-selector-challenge/app.js
@@ -2,13 +2,17 @@
 //- use .checked to check for balloons.
 
 
-
 document.getElementById("btn").addEventListener("click", calculate)
 let costEl = document.getElementById("cost-el")
- 
 
 function calculate() {
     // 1. Write the JavaScript to calculate the total cost of the selected options from the form.
     // 2. Display the total cost in the cost-el span.
+    let foodElement = document.getElementById("food-select").value
+    let transElement = document.getElementById("transport-select").value
+
+    costEl.innerHTML = parseInt(foodElement) + parseInt(transElement)
+    console.log(foodElement)
+    console.log(transElement)
 }
  
