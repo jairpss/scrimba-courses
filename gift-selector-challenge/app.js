1c4e2ca77e385a07d2b3fc7802f90d1dac267ac6
Jair Partida <jairpartida1@gmail.com>
2022-02-14 23:34:51 -0700

    starting the challenge

---
+++ gift-selector-challenge/app.js
@@ -0,0 +1,14 @@
+//- The values from the drop downs are strings, you'll need to change them to numbers.
+//- use .checked to check for balloons.
+
+
+
+document.getElementById("btn").addEventListener("click", calculate)
+let costEl = document.getElementById("cost-el")
+ 
+
+function calculate() {
+    // 1. Write the JavaScript to calculate the total cost of the selected options from the form.
+    // 2. Display the total cost in the cost-el span.
+}
+ 
